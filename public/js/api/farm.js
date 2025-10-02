3dbd26b4d825d2d0728529397d00993a73d73946
Arltriaa <pokemonrgby@gmail.com>
2025-10-02 18:04:21 +0900

    Create farm.js

---
+++ public/js/api/farm.js
@@ -0,0 +1,17 @@
+// /public/js/api/farm.js (신규 파일)
+import { func } from './firebase.js';
+import { httpsCallable } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-functions.js';
+
+const call = (name) => httpsCallable(func, name);
+
+// 씨앗 구매 (관리자용)
+export const buySeed = (data) => call('buySeed')(data);
+
+// 특정 토지의 상세 정보 (심어진 작물 등) 가져오기
+export const getFarmPlotDetail = (data) => call('getFarmPlotDetail')(data);
+
+// 특정 타일에 씨앗 심기
+export const plantSeedOnTile = (data) => call('plantSeedOnTile')(data);
+
+// 토지에 캐릭터 할당
+export const assignCharacterToFarm = (data) => call('assignCharacterToFarm')(data);
