45250fb6e1ee331dfa94d80c374f2abbc5aac62c
Arltriaa <pokemonrgby@gmail.com>
2025-10-10 06:18:10 +0900

    Update real_estate.js

--- public/js/api/real_estate.js
+++ public/js/api/real_estate.js
@@ -1,14 +1,15 @@
-// /public/js/api/real_estate.js (신규 파일)
+// /public/js/api/real_estate.js (수정)
 import { func } from './firebase.js';
 import { httpsCallable } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-functions.js';
 
-const call = (name) => httpsCallable(func, name);
+// callFn과 동일한 역할을 하는 내부 헬퍼
+const call = (name, data) => httpsCallable(func, name)(data).then(r => r.data);
 
 // 시설에 캐릭터 할당
-export const assignCharacterToFacility = (data) => call('assignCharacterToFacility')(data);
+export const assignCharacterToFacility = (data) => call('assignCharacterToFacility', data);
 
 // 새 농지 생성
-export const createFarmland = (data) => call('createFarmland')(data);
+export const createFarmland = (data) => call('createFarmland', data);
 
-// 건물 건설 시작 (미구현)
-export const startConstruction = (data) => call('startConstruction')(data);
+// 건물 건설 시작
+export const startConstruction = (data) => call('startConstruction', data);
