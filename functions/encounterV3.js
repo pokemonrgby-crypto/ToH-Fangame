831026f14fa04adb050fa640fe1aeeaa514a5840
Arltriaa <pokemonrgby@gmail.com>
2025-10-11 23:35:46 +0900

    Update encounterV3.js

--- functions/encounterV3.js
+++ functions/encounterV3.js
@@ -43,7 +43,7 @@ module.exports = (admin, { logger, GEMINI_API_KEY }) => {
 
 
 
-    const submitEncounterReview = onCall({ region: 'us-central1', secrets: [GEMINI_API_KEY] }, async (req) => {
+    const submitEncounterReview = onCall({ region: 'us-central1', secrets: [GEMINI_API_KEY], cors: true }, async (req) => {
         const uid = req.auth?.uid;
         if (!uid) throw new HttpsError('unauthenticated', '로그인이 필요합니다.');
 
